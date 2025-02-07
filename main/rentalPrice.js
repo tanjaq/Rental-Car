d2e4b83158e14be4b8d57bddec6b98a3dc689edf
maksimteterin <maksim.padriku@gmail.com>
2025-02-07 14:41:56 +0200

    Test coverage

--- main/rentalPrice.js
+++ main/rentalPrice.js
@@ -3,81 +3,100 @@ const MONTH_NO_APRIL = 3;
 const MONTH_NO_OCTOBER = 9;
 
 function calculateRentalPrice(pickupDate, dropoffDate, carType, driverAge) {
-    const rentalDays = getTotalRentalDays(pickupDate, dropoffDate);
-    const highSeason = isHighSeason(pickupDate, dropoffDate);
-    
-    if (isDriverTooYoung(driverAge)) {
-        return "Driver too young - cannot quote the price";
-    }
+  const rentalDays = getTotalRentalDays(pickupDate, dropoffDate);
+  const highSeason = isHighSeason(pickupDate, dropoffDate);
 
-    if (isRestrictedDriver(driverAge, carType)) {
-        return "Drivers 21 y/o or less can only rent Compact vehicles";
-    }
+  if (isDriverTooYoung(driverAge)) {
+    return "Driver too young - cannot quote the price";
+  }
 
-    let basePrice = calculateBaseRentalPrice(driverAge, rentalDays);
+  if (isRestrictedDriver(driverAge, carType)) {
+    return "Drivers 21 y/o or less can only rent Compact vehicles";
+  }
 
-    let rentalPrice = calculateSurcharge(basePrice, driverAge, carType, rentalDays, highSeason);
+  let basePrice = calculateBaseRentalPrice(driverAge, rentalDays);
 
-    return formatRentalPrice(rentalPrice);
+  let rentalPrice = calculateSurcharge(
+    basePrice,
+    driverAge,
+    carType,
+    rentalDays,
+    highSeason
+  );
+
+  return formatRentalPrice(rentalPrice);
 }
 
 function calculateBaseRentalPrice(driverAge, rentalDays) {
-    return driverAge * rentalDays;
+  return driverAge * rentalDays;
 }
 
-function calculateSurcharge(basePrice, driverAge, carType, rentalDays, highSeason) {
-    let rentalPrice = basePrice;
+function calculateSurcharge(
+  basePrice,
+  driverAge,
+  carType,
+  rentalDays,
+  highSeason
+) {
+  let rentalPrice = basePrice;
 
-    if (isRacerUnder25(driverAge, carType) && highSeason) {
-      rentalPrice = basePrice * 1.5;
-    }
+  if (isRacerUnder25(driverAge, carType) && highSeason) {
+    rentalPrice = basePrice * 1.5;
+  }
 
-    if (highSeason) {
-      rentalPrice = basePrice * 1.15;
-    }
+  if (!isRacerUnder25(driverAge, carType) && highSeason) {
+    rentalPrice = basePrice * 1.15;
+  }
 
-    if (isLongRental(rentalDays) && !highSeason) {
-      rentalPrice = basePrice * 0.9;
-    }
-    return rentalPrice;
+  if (isLongRental(rentalDays) && !highSeason) {
+    rentalPrice = basePrice * 0.9;
+  }
+  return rentalPrice;
 }
 
 function getTotalRentalDays(pickupDate, dropoffDate) {
-    const firstDate = new Date(pickupDate);
-    const secondDate = new Date(dropoffDate);
+  const firstDate = new Date(pickupDate);
+  const secondDate = new Date(dropoffDate);
 
-    return Math.round((secondDate - firstDate) / DAY_IN_MILLISECONDS) + 1;
+  return Math.round((secondDate - firstDate) / DAY_IN_MILLISECONDS) + 1;
 }
 
 function isHighSeason(pickupDate, dropoffDate) {
-    const pickupMonth = new Date(pickupDate).getMonth();
-    const dropoffMonth = new Date(dropoffDate).getMonth();
+  const pickupMonth = new Date(pickupDate).getMonth();
+  const dropoffMonth = new Date(dropoffDate).getMonth();
 
-    if ((pickupMonth >= MONTH_NO_APRIL && pickupMonth <= MONTH_NO_OCTOBER) ||
-        (dropoffMonth >= MONTH_NO_APRIL && dropoffMonth <= MONTH_NO_OCTOBER) ||
-        (pickupMonth < MONTH_NO_APRIL && dropoffMonth > MONTH_NO_OCTOBER)) {
-        return true;
-    }
+  if (
+    (pickupMonth >= MONTH_NO_APRIL && pickupMonth <= MONTH_NO_OCTOBER) ||
+    (dropoffMonth >= MONTH_NO_APRIL && dropoffMonth <= MONTH_NO_OCTOBER) ||
+    (pickupMonth < MONTH_NO_APRIL && dropoffMonth > MONTH_NO_OCTOBER)
+  ) {
+    return true;
+  }
+  return false;
 }
 
 function isDriverTooYoung(driverAge) {
-    return driverAge < 18;
+  return driverAge < 18;
 }
 
 function isRestrictedDriver(driverAge, carType) {
-    return driverAge <= 21 && carType !== "Compact";
+  return driverAge <= 21 && carType !== "Compact";
 }
 
 function isRacerUnder25(driverAge, carType) {
-    return driverAge <= 25 && carType === "Racer";
+  return driverAge <= 25 && carType === "Racer";
 }
 
 function isLongRental(rentalDays) {
-    return rentalDays > 10;
+  return rentalDays > 10;
 }
 
 function formatRentalPrice(rentalPrice) {
-    return 'Price: $' + rentalPrice.toFixed(2);
+  return "Price: $" + rentalPrice.toFixed(2);
 }
 
 exports.calculateRentalPrice = calculateRentalPrice;
+exports.calculateBaseRentalPrice = calculateBaseRentalPrice;
+exports.calculateSurcharge = calculateSurcharge;
+exports.isHighSeason = isHighSeason;
+exports.formatRentalPrice = formatRentalPrice;
--- test/rentalPrice.test.js
+++ test/rentalPrice.test.js
@@ -1,6 +1,47 @@
-const rental = require('../main/rentalPrice');
+const rental = require("../main/rentalPrice");
 
-test('Driver under 18 cannot rent a car', () => {
-    expect(rental.calculateRentalPrice('2024-01-01', '2024-02-02', 'Compact', 15))
-    .toBe('Driver too young - cannot quote the price');
+test("Driver under 18 cannot rent a car", () => {
+  expect(
+    rental.calculateRentalPrice("2024-01-01", "2024-02-02", "Compact", 15)
+  ).toBe("Driver too young - cannot quote the price");
+});
+
+test("Drivers 21 y/o or less can only rent Compact vehicles", () => {
+  expect(
+    rental.calculateRentalPrice("2024-01-01", "2024-02-02", "Electir", 19)
+  ).toBe("Drivers 21 y/o or less can only rent Compact vehicles");
+});
+
+test("Drivers that are 22 y.o can get a Compact car for 2 days for 44 bucks during a low season", () => {
+  expect(
+    rental.calculateRentalPrice("2024-01-01", "2024-01-02", "Compact", 22)
+  ).toBe("Price: $44.00");
+});
+
+test("A 21 y.o driver can get a car for 2 days for 42 eurs", () => {
+  expect(rental.calculateBaseRentalPrice(21, 2)).toBe(42);
+});
+
+test("A price for a racer driver that is under 25 during high season will be 1.5 times higher", () => {
+  expect(rental.calculateSurcharge(42, 24, "Racer", 2, true)).toBe(63);
+});
+
+test("A price for a racer driver that over 25 during high season will be 1.15 times higher", () => {
+  expect(rental.calculateSurcharge(42, 26, "Racer", 2, true)).toBe(48.3);
+});
+
+test("For a rental over 10 days, driver gets a 10% discound, if it is not a high season", () => {
+  expect(rental.calculateSurcharge(40, 26, "Compact", 11, false)).toBe(36);
+});
+test("High season is from april to october", () => {
+  expect(rental.isHighSeason("2024-04-12", "2024-07-00")).toBe(true);
+});
+test("High season is if car is taken from april up to october", () => {
+  expect(rental.isHighSeason("2024-04-12", "2024-07-00")).toBe(true);
+});
+test("It is not high season is if car is taken from oktober and returned before april", () => {
+  expect(rental.isHighSeason("2024-11-12", "2025-03-00")).toBe(false);
+});
+test("A price is being rounded to the second integer", () => {
+  expect(rental.formatRentalPrice(2.33)).toBe("Price: $2.33");
 });
