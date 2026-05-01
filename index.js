f68998a63c605044a381ca10c036cd36ab66f50d
sotnemfresh <98352598+sotnemfresh@users.noreply.github.com>
2026-05-01 16:24:09 +0300

    Refactored index.js to comply with checks

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const rental = require('./rentalPrice');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const rental = require("./rentalPrice");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
-app.use('/pictures', express.static('images'));
+app.use(express.static("public"));
+app.use("/pictures", express.static("images"));
 
-const formHtml = fs.readFileSync('form.html', 'utf8');
-const resultHtml = fs.readFileSync('result.html', 'utf8');
+const formHtml = fs.readFileSync("form.html", "utf8");
+const resultHtml = fs.readFileSync("result.html", "utf8");
 
-app.post('/', (req, res) => {
-    const post = req.body;
-    const result = rental.calculatePrice(
-        String(post.type),
-        Number(post.age),
-        Date.parse(post.licenseDate),
-        Date.parse(post.pickupDate),
-        Date.parse(post.dropoffDate)
-    );
-    res.send(formHtml + resultHtml.replaceAll('$0', result));
+app.post("/", (req, res) => {
+  const post = req.body;
+  const result = rental.calculatePrice(
+    String(post.type),
+    Number(post.age),
+    Date.parse(post.licenseDate),
+    Date.parse(post.pickupDate),
+    Date.parse(post.dropoffDate)
+  );
+  res.send(formHtml + resultHtml.replaceAll("$0", result));
 });
 
-app.get('/', (req, res) => {
-    res.send(formHtml);
+app.get("/", (req, res) => {
+  res.send(formHtml);
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
